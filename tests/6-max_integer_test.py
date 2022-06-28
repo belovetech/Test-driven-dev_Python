545d54a46a7d74d483efd0fdaccd06fa5ca32e23
belovetech <belovetech@gmail.com>
2022-06-28 14:11:31 +0100

    Add mas integer

--- tests/6-max_integer_test.py
+++ tests/6-max_integer_test.py
@@ -9,4 +9,13 @@ class TestMaxInteger(unittest.TestCase):
     """Test max integer"""
 
     def test_type(self):
-        self.assertEqual(max_integer)
+        l =  ["a", 1, 5, "z", 29]
+        self.assertRaises(TypeError, max_integer, (1, 4, 6, 8, 9))
+        self.assertRaises(ValueError, max_integer, l)
+        
+    def test_list(self):
+        self.assertEqual(max_integer([1, 4, 7, 8]), 8)
+        
+        
+    # def test_value(self):
+    #     self.assertRaises(ValueError, max_integer, ["a", 1, 5, "z", 29])

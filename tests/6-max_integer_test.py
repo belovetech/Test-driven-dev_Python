73a4fcf5692445abf1eca803c829eb88db8b7c70
Wasiu Bakare <wasiubakare@gmail.com>
2022-06-28 12:51:20 +0100

    Unit test for max integer

---
+++ tests/6-max_integer_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Test max integer"""
+
+    def test_type(self):
+        self.assertEqual(max_integer)
